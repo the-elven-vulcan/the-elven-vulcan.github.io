2668178877720cf813809c499b624fc7e9ad1904
the-elven-vulcan <59757842+the-elven-vulcan@users.noreply.github.com>
2020-01-11 11:29:11 +0530

    Add files via upload
    first commit

---
+++ first/scripts/main.js
@@ -0,0 +1,20 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    //console.log(myImage)
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
