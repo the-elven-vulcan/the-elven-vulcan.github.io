59eaa5260465de501b0d95461bc93c73c770e019
the-elven-vulcan <swapnil.iitm@gmail.com>
2020-01-12 21:12:17 +0530

    bug fix

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ myImage.onclick = function() {
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
-function setUserName() {
+myButton.onclick = function setUserName() {
     let myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
     myHeading.textContent = 'Mozilla is cool, ' + myName;
